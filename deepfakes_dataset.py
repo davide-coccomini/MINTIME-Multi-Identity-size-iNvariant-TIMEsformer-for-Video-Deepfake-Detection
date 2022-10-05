Fix mask generation and attention calculation when features disabled

deepfakes_dataset.py
@@ -30,7 +30,7 @@ RANGE_SIZE = 5
 SIZE_EMB_DICT = [(1+i*RANGE_SIZE, (i+1)*RANGE_SIZE) if i != 0 else (0, RANGE_SIZE) for i in range(20)]
 
 class DeepFakesDataset(Dataset):
-    def __init__(self, videos_paths, labels, data_path, video_path, image_size, augmentation = None, multiclass_labels = None, save_attention_plots = False, mode = 'train', model = 0, num_frames = 8, max_identities = 3, num_patches=49):
+    def __init__(self, videos_paths, labels, data_path, video_path, image_size, augmentation = None, multiclass_labels = None, save_attention_plots = False, mode = 'train', model = 0, num_frames = 8, max_identities = 3, num_patches=49, enable_identity_attention = True):
         self.x = videos_paths
         self.y = labels
         self.multiclass_labels = multiclass_labels
@@ -50,6 +50,7 @@ class DeepFakesDataset(Dataset):
                   2:  [int(num_frames/2), int(num_frames/2)],
                   3:  [int(num_frames/3), int(num_frames/3), int(num_frames/4)],
                   4:  [int(num_frames/3), int(num_frames/3), int(num_frames/8), int(num_frames/8)]}
+        self.enable_identity_attention = enable_identity_attention
 
     
     def create_train_transforms(self, size, additional_targets, augmentation):
@@ -223,7 +224,7 @@ class DeepFakesDataset(Dataset):
                 
                 if len(missing_faces) > 0:
                     identity_faces = identity_faces + missing_faces # Add the missing faces to the identity
-
+                
             identity_faces = np.asarray(sorted(identity_faces, key=lambda x:int(os.path.basename(x).split("_")[0])))
 
             # Select uniformly the frames in an alternate way
@@ -242,7 +243,6 @@ class DeepFakesDataset(Dataset):
             height = capture.get(4) 
             video_area = width*height/2
             identity_size_embeddings = []
-            
             for image_index, image_path in enumerate(identity_faces):
                 # Read face image
                 image = cv2.imread(image_path)
@@ -262,12 +262,14 @@ class DeepFakesDataset(Dataset):
 
 
             # If the readed faces are less than max_faces we need to add empty images and generate the mask
-            if len(identity_images) < max_faces: 
+            if len(identity_images) < max_faces:
                 diff = max_faces - len(identity_size_embeddings)
                 identity_size_embeddings = np.concatenate((identity_size_embeddings, np.zeros(diff)))
                 identity_images.extend([np.zeros((self.image_size, self.image_size, 3), dtype=np.uint8) for i in range(diff)])
-                mask.extend([1 if i < max_faces - diff else 0 for i in range(max_faces)])
                 images_frames.extend([max(images_frames) for i in range(diff)])
+
+            if self.enable_identity_attention: # Calculate attention only between faces of the same identity
+                mask.extend([1 if i < max_faces - diff else 0 for i in range(max_faces)])
             else: # Otherwise all the faces are valid
                 mask.extend([1 for i in range(max_faces)])
 
@@ -283,7 +285,8 @@ class DeepFakesDataset(Dataset):
         if self.mode == 'train':
             transform = self.create_train_transforms(self.image_size, additional_targets, self.augmentation)
         else:
-            transform = self.create_val_transform(self.image_size, additional_targets)  
+            transform = self.create_val_transform(self.image_size, additional_targets) 
+
         if len(sequence) == 8:
             transformed_images = transform(image=sequence[0], image1=sequence[1], image2=sequence[2], image3=sequence[3], image4=sequence[4], image5=sequence[5], image6=sequence[6], image7=sequence[7])
         elif len(sequence) == 16:

models/size_invariant_timesformer.py
@@ -92,7 +92,7 @@ class Attention(nn.Module):
         dim,
         dim_head = 64,
         heads = 8,
-        dropout = 0.
+        dropout = 0.,
     ):
         super().__init__()
         self.heads = heads
@@ -104,6 +104,7 @@ class Attention(nn.Module):
             nn.Linear(inner_dim, dim),
             nn.Dropout(dropout)
         )
+
 
     def forward(self, x, einops_from, einops_to, mask = None, cls_mask = None, identities_mask = None, rot_emb = None, **einops_dims):
         h = self.heads
@@ -128,10 +129,8 @@ class Attention(nn.Module):
         v_ = torch.cat((cls_v, v_), dim = 1)
 
         # attention
-        if not self.enable_identity_attention:
-            mask = [1 for i in range(len(mask))]
-            
         out, attentions = attn(q_, k_, v_, mask = mask)
+
         # merge back time or space
         out = rearrange(out, f'{einops_to} -> {einops_from}', **einops_dims)
 
@@ -170,7 +169,6 @@ class SizeInvariantTimeSformer(nn.Module):
         self.shift_tokens = config['model']['shift-tokens']
         self.enable_size_emb = config['model']['enable-size-emb']
         self.enable_pos_emb = config['model']['enable-pos-emb']
-        self.enable_identity_attention = config['model']['enable-identity-attention']
         self.require_attention = require_attention
 
         num_positions = self.num_frames * self.channels
@@ -228,15 +226,16 @@ class SizeInvariantTimeSformer(nn.Module):
         n = h * w
         x = rearrange(x, 'b f c h w -> b (f h w) c')                                   # B x F*P*P x C
         tokens = self.to_patch_embedding(x)                                            # B x 8*7*7 x dim
+
         # Add cls token
         cls_token = repeat(self.cls_token, 'n d -> b n d', b = b)
-        x =  torch.cat((cls_token, tokens), dim = 1)
+        x = torch.cat((cls_token, tokens), dim = 1)
 
         # Positional 
         if self.enable_pos_emb:
             x += self.pos_emb(positions)
         else:
-            x += self.pos_emb(torch.arange(x.shape[1]))
+            x += (self.pos_emb(torch.arange(x.shape[1]).to(device)))
 
         # Size embedding
         if self.enable_size_emb:
@@ -248,7 +247,7 @@ class SizeInvariantTimeSformer(nn.Module):
             x += self.size_emb(size_embedding)
        
                 
-        # Frame mask
+        # Frame mask            
         frame_mask = repeat(mask, 'b f1 -> b f2 f1', f2 = self.num_frames)
         frame_mask = torch.logical_and(frame_mask, identities_mask)
         frame_mask = F.pad(frame_mask, (1, 0), value= True)

train.py
@@ -35,7 +35,7 @@ from models.xception import xception
 
 
 if __name__ == "__main__":
-    os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3,4"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
     parser = argparse.ArgumentParser()
     
     parser.add_argument('--train_list_file', default="../../datasets/ForgeryNet/faces/train_and_val.csv", type=str,
@@ -237,14 +237,14 @@ if __name__ == "__main__":
     loss_fn = torch.nn.BCEWithLogitsLoss(pos_weight=torch.tensor([class_weights]))
 
     # Create the data loaders 
-    train_dataset = DeepFakesDataset(train_videos, train_labels, augmentation=config['training']['augmentation'], image_size=config['model']['image-size'], data_path=opt.data_path, video_path=opt.video_path, num_frames=config['model']['num-frames'], num_patches=num_patches, max_identities=config['model']['max-identities'])
+    train_dataset = DeepFakesDataset(train_videos, train_labels, augmentation=config['training']['augmentation'], image_size=config['model']['image-size'], data_path=opt.data_path, video_path=opt.video_path, num_frames=config['model']['num-frames'], num_patches=num_patches, max_identities=config['model']['max-identities'], enable_identity_attention=config['model']['enable-identity-attention'])
     train_dl = torch.utils.data.DataLoader(train_dataset, batch_size=config['training']['bs'], shuffle=True, sampler=None,
                                  batch_sampler=None, num_workers=opt.workers, collate_fn=None,
                                  pin_memory=False, drop_last=False, timeout=0,
                                  worker_init_fn=None, prefetch_factor=2,
                                  persistent_workers=False)
 
-    validation_dataset = DeepFakesDataset(validation_videos, validation_labels, image_size=config['model']['image-size'], data_path=opt.data_path, video_path=opt.video_path, num_frames=config['model']['num-frames'], num_patches=num_patches, max_identities=config['model']['max-identities'], mode='val')
+    validation_dataset = DeepFakesDataset(validation_videos, validation_labels, image_size=config['model']['image-size'], data_path=opt.data_path, video_path=opt.video_path, num_frames=config['model']['num-frames'], num_patches=num_patches, max_identities=config['model']['max-identities'], enable_identity_attention=config['model']['enable-identity-attention'], mode='val')
     val_dl = torch.utils.data.DataLoader(validation_dataset, batch_size=config['training']['val_bs'], shuffle=True, sampler=None,
                                     batch_sampler=None, num_workers=opt.workers, collate_fn=None,
                                     pin_memory=False, drop_last=False, timeout=0,